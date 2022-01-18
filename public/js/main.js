07ea559cc6ded4801ff2df34863df4e97044a065
Valentin Bilyk <visorien.flame@gmail.com>
2022-01-19 01:04:18 +0200

    Save receipt from site

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,15 @@ const receiptsPayerSelect = document.getElementById("receipts_payer_select")
 const paymentsAmountInput = document.getElementById("payments_amount_input")
 const savePaymentButton = document.getElementById("save_payment_button")
 
+const receiptsAmountInput = document.getElementById("receipts_amount_input")
+const receiptsDescriptionInput = document.getElementById("receipts_description_input")
+const receiptDebtorsContainer = document.getElementById("receipt_debtors_container")
+const addReceiptButton = document.getElementById("add_receipt_button")
+
 let users = []
 
 savePaymentButton.addEventListener('click', savePayment)
+addReceiptButton.addEventListener('click', saveReceipt)
 
 init()
 async function init() {
@@ -14,6 +20,7 @@ async function init() {
     renderUsersSelect(paymentsFromUserSelect)
     renderUsersSelect(paymentsToUserSelect)
     renderUsersSelect(receiptsPayerSelect)
+    renderDebtors()
 }
 
 async function getUsers() {
@@ -33,6 +40,18 @@ function renderUsersSelect(selectElement) {
         selectHtml += `<option ${i == 0 ? 'selected': ''} value="${users[i].id}">${users[i].name}</option>`
     }
     selectElement.innerHTML = selectHtml
+}
+
+function renderDebtors() {
+    let debtorsHtml = ``
+    for (let i = 0; i < users.length; i++) {
+        debtorsHtml += `<div class="debtor">
+        <input class="debtor_checkbox" type="checkbox" checked id="debtor${i}" name="debtor${i}" value="${users[i].id}">
+        <label for="debtor${i}">${users[i].name}</label>
+        <input class="debt_amount" type="number" placeholder="0.00">
+    </div>`
+    }
+    receiptDebtorsContainer.innerHTML = debtorsHtml
 }
 
 function savePayment() {
@@ -57,6 +76,48 @@ function savePayment() {
     })
     .then((data) => {
 		console.log('add new payment: ', data)
+    })
+    .catch(e => console.error(e))
+}
+
+
+function saveReceipt() {
+    const amount = receiptsAmountInput.value
+    if(!amount) return
+
+    let debts = []
+
+    const debtors = receiptDebtorsContainer.querySelectorAll(".debtor")
+    for (let i = 0; i < debtors.length; i++) {
+        const debtorCheckbox = debtors[i].querySelector(".debtor_checkbox")
+        if(debtorCheckbox.checked) {
+            debts.push({
+                debtorId: debtorCheckbox.value,
+                amount: Number(debtors[i].querySelector(".debt_amount").value)
+            })
+        }
+        
+    }
+
+    const receipt = {
+        payerId: receiptsPayerSelect.value,
+        amount,
+        description: receiptsDescriptionInput.value,
+        debts
+	}
+
+    fetch('/receipts', {
+        method: 'POST',
+        body: JSON.stringify(receipt),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+		console.log('add new receipt: ', data)
     })
     .catch(e => console.error(e))
 }
