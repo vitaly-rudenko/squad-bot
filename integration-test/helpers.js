07fe7701b183d9274fd02d1cd22245e98048bb4b
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-01-29 00:32:41 +0200

    Improved tests

---
+++ integration-test/helpers.js
@@ -0,0 +1,137 @@
+import { expect } from 'chai'
+import fetch, { FormData, File } from 'node-fetch'
+import crypto from 'crypto'
+import jwt from 'jsonwebtoken'
+import { TOKEN_SECRET } from './env.js'
+
+const createStringGenerator = (prefix = '') => {
+  return () => prefix + crypto.randomBytes(10).toString('hex');
+}
+
+const generateUserId = createStringGenerator()
+
+export function validateResponse(response) {
+  if (response.status !== 200) {
+    throw new Error(`Invalid response: ${response.status} (${response.statusText})`)
+  }
+}
+
+export async function createUser(index = null) {
+  const id = [index, generateUserId()].filter(Boolean).join('_')
+  const user = {
+    id,
+    name: `User ${id}`,
+    username: `username_${id}`,
+  }
+
+  const response = await fetch('http://localhost:3001/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user)
+  })
+
+  validateResponse(response)
+
+  return user
+}
+
+export async function createUsers(count = 1) {
+  const users = []
+
+  for (let i = 1; i <= count; i++) {
+    users.push(await createUser(i))
+  }
+
+  return users
+}
+
+export async function createReceipt(payerId, debtsMap, { photo = null, mime = null, description = null } = {}) {
+  const debts = Object.entries(debtsMap).map(([debtorId, amount]) => ({ debtorId, amount }))
+
+  const receipt = {
+    payerId,
+    photo,
+    mime,
+    description,
+    amount: debts.reduce((acc, curr) => acc + curr.amount, 0),
+    debts,
+  }
+
+  const body = new FormData()
+  body.set('payer_id', payerId)
+  body.set('amount', debts.reduce((acc, curr) => acc + curr.amount, 0))
+  body.set('debts', JSON.stringify(debts))
+
+  if (description) {
+    body.set('description', description)
+  }
+  if (photo) {
+    const photoFile = new File([photo], 'photo.jpg', { type: mime })
+    body.set('photo', photoFile, 'photo.jpg')
+  }
+
+  const response = await fetch('http://localhost:3001/receipts', {
+    method: 'POST',
+    body,
+  })
+
+  validateResponse(response)
+
+  return receipt
+}
+
+export async function getReceipts(userId) {
+  const token = createToken(userId)
+  const response = await fetch(`http://localhost:3001/receipts?token=${token}`)
+
+  validateResponse(response)
+
+  return await response.json()
+}
+
+export function expectReceiptsToEqual(receipts1, receipts2) {
+  expect(receipts1).to.have.lengthOf(receipts2.length)
+
+  for (const [i, receipt] of receipts1.entries()) {
+    expectReceiptToShallowEqual(receipt, receipts2[i])
+  }
+}
+
+export function expectReceiptToShallowEqual(receipt1, receipt2) {
+  const { id, createdAt, ...receipt } = receipt1
+  expect(id).to.be.a.string
+  expect(Date.parse(createdAt)).to.be.greaterThanOrEqual(Date.now() - 10000)
+  expect(receipt).to.deep.equalInAnyOrder(receipt2)
+}
+
+export function createToken(userId) {
+  return jwt.sign({ userId }, TOKEN_SECRET)
+}
+
+export async function createPayment(fromUserId, toUserId, amount) {
+  const payment = { fromUserId, toUserId, amount }
+  
+  const response = await fetch('http://localhost:3001/payments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payment)
+  })
+
+  validateResponse(response)
+
+  return payment
+}
+
+export async function getDebts(userId) {
+  const response = await fetch(`http://localhost:3001/debts/${userId}`)
+
+  validateResponse(response)
+
+  return await response.json()
+}
+
+export const NO_DEBTS = { ingoingDebts: [], outgoingDebts: [] }
--- integration-test/receipts.test.js
+++ integration-test/receipts.test.js
@@ -1,159 +1,76 @@
 import chai, { expect } from 'chai'
 import deepEqualInAnyOrder from 'deep-equal-in-any-order'
-import fetch from 'node-fetch'
-import crypto from 'crypto'
-import jwt from 'jsonwebtoken'
-import { TOKEN_SECRET } from './env.js'
+import { createUsers, getReceipts, createReceipt, expectReceiptsToEqual } from './helpers.js'
 
 chai.use(deepEqualInAnyOrder)
 
-const createStringGenerator = (prefix = '') => {
-  return () => prefix + crypto.randomBytes(10).toString('hex');
-}
-
-const generateUserId = createStringGenerator()
-
-function validateResponse(response) {
-  if (response.status !== 200) {
-    throw new Error(`Invalid response: ${response.status} (${response.statusText})`)
-  }
-}
-
-async function createUser(index = null) {
-  const id = [index, generateUserId()].filter(Boolean).join('_')
-  const user = {
-    id,
-    name: `User ${id}`,
-    username: `username_${id}`,
-  }
-
-  const response = await fetch('http://localhost:3001/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(user)
-  })
-
-  validateResponse(response)
-
-  return user
-}
-
-async function createUsers(count) {
-  const users = []
-
-  for (let i = 1; i <= count; i++) {
-    users.push(await createUser(i))
-  }
-
-  return users
-}
-
-async function createReceipt(payerId, debtsMap) {
-  const debts = Object.entries(debtsMap).map(([debtorId, amount]) => ({ debtorId, amount }))
-
-  const receipt = {
-    payerId,
-    description: 'hello world',
-    amount: debts.reduce((acc, curr) => acc + curr.amount, 0),
-    debts,
-  }
-
-  const response = await fetch('http://localhost:3001/receipts', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(receipt)
-  })
-
-  validateResponse(response)
-
-  return receipt
-}
-
-async function getReceipts(userId) {
-  const token = createToken(userId)
-  const response = await fetch(`http://localhost:3001/receipts?token=${token}`)
-
-  validateResponse(response)
-
-  return await response.json()
-}
-
-function expectReceiptsToEqual(receipts1, receipts2) {
-  expect(receipts1).to.have.lengthOf(receipts2.length)
-
-  for (const [i, receipt] of receipts1.entries()) {
-    expectReceiptToShallowEqual(receipt, receipts2[i])
-  }
-}
-
-function expectReceiptToShallowEqual(receipt1, receipt2) {
-  const { id, createdAt, ...receipt } = receipt1
-  expect(id).to.be.a.string
-  expect(Date.parse(createdAt)).to.be.greaterThanOrEqual(Date.now() - 10000)
-  expect(receipt).to.deep.equalInAnyOrder(receipt2)
-}
-
-function createToken(userId) {
-  return jwt.sign({ userId }, TOKEN_SECRET)
-}
-
 describe('[receipts]', () => {
-  it('should create a receipt (1)', async () => {
-    const [user1, user2, user3, user4] = await createUsers(4)
+  describe('POST /receipts', () => {
+    it('should upload receipt photo', async () => {
+      const [user] = await createUsers(1)
 
-    expect(await getReceipts(user1.id)).to.deep.equalInAnyOrder([])
-    expect(await getReceipts(user2.id)).to.deep.equalInAnyOrder([])
-    expect(await getReceipts(user3.id)).to.deep.equalInAnyOrder([])
-    expect(await getReceipts(user4.id)).to.deep.equalInAnyOrder([])
-
-    await createReceipt(user1.id, {
-      [user1.id]: 5,
-      [user2.id]: 15,
+      await createReceipt(user.id, {
+        [user.id]: 1,
+      }, { photo: Buffer.from('hello world'), mime: 'image/jpeg' })
     })
+  })
 
-    const receipt1 = {
-      payerId: user1.id,
-      description: 'hello world',
-      amount: 20,
-      debts: [{
-        debtorId: user1.id,
-        amount: 5,
-      }, {
-        debtorId: user2.id,
-        amount: 15,
-      }]
-    }
-
-    expectReceiptsToEqual(await getReceipts(user1.id), [receipt1])
-
-    expect(await getReceipts(user2.id)).to.deep.equalInAnyOrder(await getReceipts(user1.id))
-    expect(await getReceipts(user3.id)).to.deep.equalInAnyOrder([])
-
-    await createReceipt(user3.id, {
-      [user1.id]: 50,
-      [user3.id]: 50,
+  describe('GET /receipts', () => {
+    it('should returns receipts by user ID', async () => {
+      const [user1, user2, user3, user4] = await createUsers(4)
+  
+      expect(await getReceipts(user1.id)).to.deep.equalInAnyOrder([])
+      expect(await getReceipts(user2.id)).to.deep.equalInAnyOrder([])
+      expect(await getReceipts(user3.id)).to.deep.equalInAnyOrder([])
+      expect(await getReceipts(user4.id)).to.deep.equalInAnyOrder([])
+  
+      await createReceipt(user1.id, {
+        [user1.id]: 5,
+        [user2.id]: 15,
+      }, { photo: Buffer.from('hello world') })
+  
+      const receipt1 = {
+        payerId: user1.id,
+        amount: 20,
+        hasPhoto: true,
+        description: null,
+        debts: [{
+          debtorId: user1.id,
+          amount: 5,
+        }, {
+          debtorId: user2.id,
+          amount: 15,
+        }]
+      }
+  
+      expectReceiptsToEqual(await getReceipts(user1.id), [receipt1])
+  
+      expect(await getReceipts(user2.id)).to.deep.equalInAnyOrder(await getReceipts(user1.id))
+      expect(await getReceipts(user3.id)).to.deep.equalInAnyOrder([])
+  
+      await createReceipt(user3.id, {
+        [user1.id]: 50,
+        [user3.id]: 50,
+      }, { description: 'hello world' })
+  
+      const receipt2 = {
+        payerId: user3.id,
+        hasPhoto: false,
+        description: 'hello world',
+        amount: 100,
+        debts: [{
+          debtorId: user1.id,
+          amount: 50,
+        }, {
+          debtorId: user3.id,
+          amount: 50,
+        }]
+      }
+  
+      expectReceiptsToEqual(await getReceipts(user1.id), [receipt2, receipt1])
+      expectReceiptsToEqual(await getReceipts(user2.id), [receipt1])
+      expectReceiptsToEqual(await getReceipts(user3.id), [receipt2])
+      expect(await getReceipts(user4.id)).to.deep.equalInAnyOrder([])
     })
-
-    const receipt2 = {
-      payerId: user3.id,
-      description: 'hello world',
-      amount: 100,
-      debts: [{
-        debtorId: user1.id,
-        amount: 50,
-      }, {
-        debtorId: user3.id,
-        amount: 50,
-      }]
-    }
-
-    expectReceiptsToEqual(await getReceipts(user1.id), [receipt2, receipt1])
-    expectReceiptsToEqual(await getReceipts(user2.id), [receipt1])
-    expectReceiptsToEqual(await getReceipts(user3.id), [receipt2])
-    expect(await getReceipts(user4.id)).to.deep.equalInAnyOrder([])
   })
 })
