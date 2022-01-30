56fc443299e3f4a8264452bd5e2ef74d2cf36411
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-01-30 20:40:38 +0200

    Better /debts output

--- app/flows/debts.js
+++ app/flows/debts.js
@@ -11,22 +11,22 @@ export function debtsCommand({ storage, getDebtsByUserId }) {
       return user ? user.name : `??? (${id})`
     }
 
-    const ingoingDebtsFormatted = ingoingDebts.length > 0 ? `
-Тебе должны:
+    const ingoingDebtsFormatted = ingoingDebts.length > 0 ? `\
+🙂 Тебе должны:
 ${
   ingoingDebts
     .map(debt => `- ${getUserName(debt.userId)}: ${renderMoney(debt.amount)} грн`)
     .join('\n')
-}` : 'Тебе никто ничего не должен 🙂'
+}` : 'Тебе никто ничего не должен 🙁'
 
-const outgoingDebtsFormatted = outgoingDebts.length > 0 ? `
-Ты должен:
+const outgoingDebtsFormatted = outgoingDebts.length > 0 ? `\
+🙁 Ты должен:
 ${
   outgoingDebts
     .map(debt => `- ${getUserName(debt.userId)}: ${renderMoney(debt.amount)} грн`)
     .join('\n')
 }` : 'Ты никому ничего не должен 🙂'
 
-    await context.reply([outgoingDebtsFormatted, ingoingDebtsFormatted].filter(Boolean).join('\n'))
+    await context.reply([outgoingDebtsFormatted, ingoingDebtsFormatted].filter(Boolean).join('\n\n'))
   }
 }
