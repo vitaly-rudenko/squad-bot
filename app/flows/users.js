34868baf0af47e477869f81a480f09d7d2dc568e
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-01-30 20:57:38 +0200

    Better /users output

--- app/flows/users.js
+++ app/flows/users.js
@@ -1,14 +1,12 @@
-import { stripIndent } from 'common-tags'
-
 export function usersCommand({ storage }) {
   return async (context) => {
     const users = await storage.findUsers()
 
     await context.reply(`
-Архивчане:
+👥 Пользователи:
 ${
   users
-    .map(user => `- ${user.name} (username: ${user.username}, id: ${user.id})`)
+    .map(user => `- ${user.name} (ник: ${user.username}, id: ${user.id})`)
     .join('\n') || '- Никто не зарегистрирован :('
 }
     `)
