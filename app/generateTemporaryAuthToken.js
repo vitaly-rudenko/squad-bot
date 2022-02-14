5bf14607de014844dcb0707f0f7773d56375a4d4
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-02-14 18:24:03 +0200

    Formatting change

--- app/generateTemporaryAuthToken.js
+++ app/generateTemporaryAuthToken.js
@@ -1,7 +1,5 @@
 import jwt from 'jsonwebtoken'
 
 export function generateTemporaryAuthToken(userId) {
-  return jwt.sign({ userId }, process.env.TOKEN_SECRET, {
-    expiresIn: '1 minute',
-  })
+  return jwt.sign({ userId }, process.env.TOKEN_SECRET, { expiresIn: '1 minute' })
 }
