9c2064172489c0df2e4514ab86dcab9bfbec19f6
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-03-10 16:28:07 +0200

    Added info() to logger

--- app/shared/TelegramLogger.js
+++ app/shared/TelegramLogger.js
@@ -3,17 +3,17 @@ export class TelegramLogger {
     this._debugChatId = debugChatId
     this._telegram = telegram
   }
-  
-  async error(error) {
+
+  info(message) {
+    console.log(message)
+  }
+
+  error(error) {
     console.error('Unexpected error:', error)
 
-    try {
-      await this._telegram.sendMessage(
-        this._debugChatId,
-        `❗️Unexpected error at ${new Date().toISOString()}❗️\n${error.name}: ${error.message}\n\nStack:\n${error.stack}`
-      )
-    } catch (error) {
-      console.warn('Could not post log to debug chat:', error)
-    }
+    this._telegram.sendMessage(
+      this._debugChatId,
+      `❗️Unexpected error at ${new Date().toISOString()}❗️\n${error.name}: ${error.message}\n\nStack:\n${error.stack}`
+    ).catch(error => console.warn('Could not log to the debug chat:', error))
   }
 }
