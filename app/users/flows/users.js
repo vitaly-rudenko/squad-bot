52958e9a6c3e004092561530ea4f39da16ded14c
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-03-18 23:34:50 +0200

    Shows registered /users of current group chat only

--- app/users/flows/users.js
+++ app/users/flows/users.js
@@ -1,9 +1,17 @@
 import { escapeMd } from '../../utils/escapeMd.js'
 
-export function usersCommand({ usersStorage }) {
+export function usersCommand({ usersStorage, membershipStorage }) {
   return async (context) => {
-    const { localize } = context.state
-    const users = await usersStorage.findAll()
+    const { localize, chatId } = context.state
+    const isPrivateChat = context.chat.type === 'private'
+
+    let users
+    if (isPrivateChat) {
+      users = await usersStorage.findAll()
+    } else {
+      const userIds = await membershipStorage.findUserIdsByChatId(chatId)
+      users = await usersStorage.findByIds(userIds)
+    }
 
     function localizeUser(user) {
       return localize('command.users.user', {
@@ -20,7 +28,7 @@ export function usersCommand({ usersStorage }) {
     }
 
     await context.reply(
-      localize('command.users.users', {
+      localize(isPrivateChat ? 'command.users.allUsers' : 'command.users.chatUsers', {
         users: users.map(user => localizeUser(user)).join('\n'),
       }),
       { parse_mode: 'MarkdownV2' }
