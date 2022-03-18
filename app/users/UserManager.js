978b21821835f209b7fb91cb540c503344ef0192
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-03-18 23:40:16 +0200

    Added hard and soft register methods to UserManager

--- app/users/UserManager.js
+++ app/users/UserManager.js
@@ -4,8 +4,52 @@ export class UserManager {
     this._userCache = userCache
   }
 
-  clearCache(userId) {
-    this._userCache.delete(userId)
+  /** @param {import('./User').User} user */
+  async softRegister(user) {
+    const existingUser = await this.getCachedUser(user.id)
+
+    if (existingUser) {
+      if (!existingUser.isComplete && user.isComplete) {
+        await this._usersStorage.update(user)
+        await this.clearCache(user.id)
+
+        console.log(`User is now complete: ${user.name} (${user.id}, @${user.username})`)
+      }
+    } else {
+      try {
+        await this._usersStorage.create(user)
+        await this.clearCache(user.id)
+
+        console.log(`User has been registered: ${user.name} (${user.id}, @${user.username}, complete: ${user.isComplete})`)
+      } catch (error) {
+        if (error.code !== 'ALREADY_EXISTS') {
+          throw error
+        }
+      }
+    }
+  }
+
+  /** @param {import('./User').User} user */
+  async hardRegister(user) {
+    let isNew = false
+
+    try {
+      await this._usersStorage.create(user)
+      isNew = true
+    } catch (error) {
+      if (error.code === 'ALREADY_EXISTS') {
+        await this._usersStorage.update(user)
+      } else {
+        throw error
+      }
+    }
+
+    await this.clearCache(user.id)
+    return isNew
+  }
+
+  async clearCache(userId) {
+    await this._userCache.delete(userId)
   }
 
   async getCachedUser(userId) {
@@ -16,7 +60,7 @@ export class UserManager {
 
     const user = await this._usersStorage.findById(userId)
     if (user) {
-      this._userCache.cache(user)
+      await this._userCache.cache(user)
     }
 
     return user
