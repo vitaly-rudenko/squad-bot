841fa3255842408aaadceea1e50e3cb09a88f353
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-07-04 01:26:42 +0300

    Fixed /rollcalls users formatting

--- app/rollcalls/flows/rollcalls.js
+++ app/rollcalls/flows/rollcalls.js
@@ -14,6 +14,7 @@ export function rollCallsCommand({ rollCallsStorage, usersStorage, userSessionMa
       rollCalls
         .filter(rc => rc.usersPattern !== '*')
         .map(rc => rc.usersPattern.split(','))
+        .reduce((acc, curr) => [...acc, ...curr], [])
     )]
 
     const users = await usersStorage.findByIds(userIds)
