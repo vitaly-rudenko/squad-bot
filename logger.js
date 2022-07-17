b85953a39abec86e1a8faab2927bbca70aab8f57
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-07-17 23:38:42 +0300

    Improved logging

--- logger.js
+++ logger.js
@@ -1,6 +1,10 @@
 import pino from 'pino'
+import { logLevel } from './env.js'
+
+console.log('Log level:', logLevel)
 
 export const logger = pino({
+  level: logLevel,
   transport: {
     target: 'pino-pretty',
     options: {
