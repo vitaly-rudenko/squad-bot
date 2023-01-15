3704535e668ed734be28330946c94ca07a4473c7
Vitaly Rudenko <maurice.dederick@gmail.com>
2023-01-15 23:12:35 +0200

    Minor fixes

--- app/memberships/RefreshMembershipsUseCase.js
+++ app/memberships/RefreshMembershipsUseCase.js
@@ -1,4 +1,4 @@
-import { logger } from '../../logger'
+import { logger } from '../../logger.js'
 
 export class RefreshMembershipsUseCase {
   constructor({ membershipStorage, membershipManager, errorLogger }) {
