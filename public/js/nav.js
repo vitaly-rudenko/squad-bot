cca6c842f38599e4711b278f17eeae9d23b07ced
Vitaly Rudenko <maurice.dederick@gmail.com>
2023-11-29 23:04:34 +0200

    Improved navbar

--- public/js/nav.js
+++ public/js/nav.js
@@ -6,10 +6,10 @@ const navHtml = `<input id="toggle" type="checkbox">
 </label>
 
 <nav class="nav">
-    <a rel="nofollow" rel="noreferrer" class="nav-item" href="/">Додати чек</a>
-    <a rel="nofollow" rel="noreferrer" class="nav-item" href="/paymentview">Додати платіж</a>
-    <a rel="nofollow" rel="noreferrer" class="nav-item" href="/receiptslist">Усі чеки</a>
-    <a rel="nofollow" rel="noreferrer" class="nav-item" href="/paymentslist">Усі платежі</a>
+    <a rel="nofollow" rel="noreferrer" class="nav-item" href="/">Створити чек</a>
+    <a rel="nofollow" rel="noreferrer" class="nav-item nav-item--separated" href="/receiptslist">Переглянути чеки</a>
+    <a rel="nofollow" rel="noreferrer" class="nav-item" href="/paymentview">Створити платіж</a>
+    <a rel="nofollow" rel="noreferrer" class="nav-item" href="/paymentslist">Переглянути платежі</a>
 </nav>`
 
 navContainer.innerHTML = navHtml
