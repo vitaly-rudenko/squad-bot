a9812956fd14e18cbf3187bcc1cfda0c3258f70a
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-02-03 23:59:11 +0200

    Added API for getting and updating admins (titles)

--- app.js
+++ app.js
@@ -710,6 +710,126 @@ async function start() {
     res.json(await groupStorage.findByMemberUserId(req.user.id))
   })
 
+  router.get('/admins', async (req, res) => {
+    const groupId = req.query.group_id
+    if (typeof groupId !== 'string') {
+      res.sendStatus(400)
+      return
+    }
+
+    if (!(await membershipManager.isHardLinked(req.user.id, groupId))) {
+      res.sendStatus(403)
+      return
+    }
+
+    try {
+      const admins = await bot.telegram.getChatAdministrators(Number(groupId))
+
+      res.json(admins.map(admin => ({
+        userId: String(admin.user.id),
+        title: admin.custom_title || '',
+        isCreator: admin.status === 'creator',
+        isCurrentBot: admin.user.id === botInfo.id,
+        editable: admin.status !== 'creator' && admin.can_be_edited,
+      })))
+    } catch (error) {
+      if (error.message.includes('chat not found')) {
+        res.status(502).json({ error: { code: 'CHAT_NOT_FOUND', message: 'Chat not found. Does bot have access to the group?' } })
+        return
+      }
+
+      logger.error({ error, groupId }, 'Could not get chat administrators')
+      throw error
+    }
+  })
+
+  async function setUserCustomTitle(groupId, userId, title) {
+    try {
+      await bot.telegram.setChatAdministratorCustomTitle(Number(groupId), Number(userId), title)
+    } catch (error) {
+      if (error.message.includes('ADMIN_RANK_EMOJI_NOT_ALLOWED')) {
+        throw new ApiError({ code: 'INVALID_CUSTOM_TITLE', status: 502 })
+      }
+
+      if (error.message.includes('RIGHT_FORBIDDEN')) {
+        throw new ApiError({ code: 'INSUFFICIENT_PERMISSIONS', status: 502 })
+      }
+
+      if (error.message.includes('method is available only for supergroups')) {
+        throw new ApiError({ code: 'FOR_SUPERGROUPS_ONLY', status: 502 })
+      }
+
+      throw error
+    }
+  }
+
+  // https://stackoverflow.com/questions/61022534/telegram-bot-with-add-new-admin-rights-cant-promote-new-admins-or-change-cust
+  // "To change custom title, user has to be promoted by the bot itself."
+  async function setUserCustomTitleAndPromote(groupId, userId, title) {
+    try {
+      await setUserCustomTitle(groupId, userId, title)
+    } catch (error) {
+      if (!error.message.includes('user is not an administrator')) {
+        throw error
+      }
+
+      try {
+        await bot.telegram.promoteChatMember(Number(groupId), Number(userId), {
+          can_change_info: true,
+          can_pin_messages: true,
+        })
+      } catch (error) {
+        if (error.message.includes('not enough rights')) {
+          throw new ApiError({ code: 'CANNOT_ADD_NEW_ADMINS', status: 502 })
+        }
+
+        throw error
+      }
+
+      await setUserCustomTitle(groupId, userId, title)
+    }
+  }
+
+  router.patch('/admins', async (req, res) => {
+    const { groupId, admins } = req.body
+    if (
+      typeof groupId !== 'string' ||
+      !Array.isArray(admins) ||
+      admins.some(admin =>
+        typeof admin.userId !== 'string' ||
+        typeof admin.title !== 'string'
+      )
+    ) {
+      res.sendStatus(400)
+      return
+    }
+
+    if (!(await membershipManager.isHardLinked(req.user.id, groupId))) {
+      res.sendStatus(403)
+      return
+    }
+
+    const errorCodes = []
+    for (const admin of admins) {
+      try {
+        await setUserCustomTitleAndPromote(groupId, admin.userId, admin.title)
+      } catch (error) {
+        if (error instanceof ApiError) {
+          errorCodes.push({ userId: admin.userId, errorCode: error.code })
+        } else {
+          logger.error({ error, groupId, admin }, 'Could not update admin')
+          throw error
+        }
+      }
+    }
+
+    if (errorCodes.length > 0) {
+      res.status(400).json({ error: { code: 'COULD_NOT_UPDATE_ADMINS', context: { errorCodes } } })
+    } else {
+      res.sendStatus(200)
+    }
+  })
+
   app.use(router)
 
   const port = Number(process.env.PORT) || 3000
