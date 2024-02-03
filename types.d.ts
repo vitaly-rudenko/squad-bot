84fc110a2c14e3bda7fbd9354fc2295ad4518d87
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-02-03 13:38:32 +0200

    Minor type fixes

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,10 @@
-import { User } from './app/users/User';
+import { User } from './app/users/User.js';
 
 declare global {
   namespace Express {
     export interface Request {
       user: User;
+      file?: Multer.File;
     }
   }
 }
